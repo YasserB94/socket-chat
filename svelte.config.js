77ac1297a21726074fb309067265894c73246b30
Yasser Bal <yasserbal@icloud.com>
2022-07-22 17:09:30 +0200

    import preprocess module to sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
-
+import preprocess from 'svelte/types/compiler/preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		target: '#svelte'
 	}
 };
 
